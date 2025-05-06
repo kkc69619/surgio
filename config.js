4720756b45a23bdb79adba6cf4e0f3aebadf651c
𝐤𝐤𝐜𝟔𝟗𝟔𝟏𝟗 <kkc69619@gmail.com>
2025-05-07 02:00:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
-module.exports = require('./build/config')
+module.exports = {
+  urlBase: 'https://surge.vercel.app/', // 后续部署后替换
+  artifacts: [
+    {
+      name: 'Surge.conf',
+      template: 'surge',
+      provider: 'example', // 对应 provider/example.js
+    },
+  ],
+};
