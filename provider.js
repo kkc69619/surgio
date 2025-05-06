b9894f49da33299b48895a2fecfb70f15ca2dcd6
𝐤𝐤𝐜𝟔𝟗𝟔𝟏𝟗 <kkc69619@gmail.com>
2025-05-07 01:59:37 +0800

    Update provider.js

--- provider.js
+++ provider.js
@@ -1,2 +1,6 @@
-module.exports = require('./build/provider')
 const { defineProvider } = require('surgio');
+
+module.exports = defineProvider({
+  url: 'https://你的订阅链接', // 替换此处
+  type: 'surge_json', // 或 v2rayn_json/trojan
+});
